157e51aed81f38613943cd8c58060a8d4f564c18
Ярослав П’ятницький <yaroslaviatnytskyi@MacBook-Air-Aroslav.local>
2022-08-10 13:45:44 +0300

    hw-05

--- src/app/App.js
+++ src/app/App.js
@@ -3,10 +3,20 @@ import Users from "./components/users";
 import api from "./api";
 
 function App() {
-    const [users, setUsers] = useState();
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
-        api.users.default.fetchAll().then((data) => setUsers(data));
+        setLoading(true);
+        api.users.default
+            .fetchAll()
+            .then((data) => setUsers(data))
+            .finally(() => setLoading(false));
     }, []);
+
+    if (loading) {
+        return "Loading...";
+    }
+
     const handleDelete = (userId) => {
         setUsers(users.filter((user) => user._id !== userId));
     };
