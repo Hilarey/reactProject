89916062ef7fe9546901f160ae834c2677fce970
Ярослав П’ятницький <yaroslaviatnytskyi@MacBook-Air-Aroslav.local>
2022-08-12 13:57:54 +0300

    hw-06

---
+++ src/app/components/tableHeader.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TableHeader = ({ onSort, selectedSort, columns }) => {
+    const handleSort = (item) => {
+        if (selectedSort.path === item) {
+            onSort({
+                ...selectedSort,
+                order: selectedSort.order === "asc" ? "desc" : "asc"
+            });
+        } else {
+            onSort({ path: item, order: "asc" });
+        }
+    };
+
+    const handleFill = (item) => {
+        if (
+            selectedSort.order === "asc" &&
+            selectedSort.path === item &&
+            item !== undefined
+        ) {
+            return "bi bi-caret-down-fill";
+        }
+        if (
+            selectedSort.order === "desc" &&
+            selectedSort.path === item &&
+            item !== undefined
+        ) {
+            return "bi bi-caret-up-fill";
+        }
+    };
+
+    return (
+        <thead>
+            <tr>
+                {Object.keys(columns).map((column) => (
+                    <th
+                        key={column}
+                        onClick={
+                            columns[column].path
+                                ? () => handleSort(columns[column].path)
+                                : undefined
+                        }
+                        {...{ role: columns[column].path && "button" }}
+                        scope="col"
+                    >
+                        {columns[column].name}
+                        <span
+                            className={handleFill(columns[column].path)}
+                        ></span>
+                    </th>
+                ))}
+            </tr>
+        </thead>
+    );
+};
+
+TableHeader.propTypes = {
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired,
+    columns: PropTypes.object.isRequired
+};
+
+export default TableHeader;
